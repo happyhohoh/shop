ea6331e0757bcf0ee0bc05dfa92920f6ded65464
Fyh <1643591770@qq.com>
2024-08-05 20:47:18 +0800

    Home组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,6 +15,16 @@ import HomeProduct from './components/HomeProduct.vue'
         <HomeNew />
         <HomeHot />
         <HomeProduct />
+        <HomePanel title="新鲜好物" subtitle="新鲜好物，好多商品"> 
+          <div>
+            我是新鲜好物的插槽内容
+          </div>
+        </HomePanel>
+        <HomePanel title="人气推荐" subtitle="人气推荐，好多商品">
+          <div>
+            我是新鲜好物的插槽内容
+          </div>
+        </HomePanel>
     </div>
   </div>
   
